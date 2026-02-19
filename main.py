92bd3ad53a0d9c2c048b8ca449b14274049e44bf
kimjarvis <kim.jarvis@tpfsystems.com>
2026-02-19 10:01:04 +0000

    Compose functions and refine

--- main.py
+++ main.py
@@ -1,50 +1,21 @@
-from ensure_balanced_parentheses import ensure_balanced_parentheses
-from parse_parentheses import parse_parentheses
-from isolate_blocks import isolate_blocks
-from process_chunks import process_chunks
-from validate_chunks import ChunkValidator
-from execute import execute
-from reconstruct import reconstruct
 from filters import Uppercase, Begin, End
-
-
-import pprint
-
-def transclude(input: str, open_parentheses: str = "{{", close_parentheses: str = "}}") -> tuple[bool, str]:
-    ensure_balanced_parentheses(input,  open_parentheses, close_parentheses)
-    a = parse_parentheses(input, open_parentheses, close_parentheses)
-    print(a)
-    b = isolate_blocks(a)
-    print(b)
-    c = process_chunks(b)
-    print(c)
-    v = ChunkValidator()
-    v.register("Uppercase", Uppercase)
-    v.register("Begin", Begin)
-    v.register("End", End)
-    d = v.validate_chunks(c)
-    print(d)
-    # pprint.pp(d)
-    e = execute(d)
-    print(e)
-    c,f = reconstruct(e, open_parentheses, close_parentheses)
-    print(f)
-    return c,f
-
-
-
+from transclude import Transclude
 
 def main():
     print("Transclusion")
 
-    transclude("""
+    t = Transclude()
+    t.register("Uppercase", Uppercase)
+    t.register("Begin", Begin)
+    t.register("End", End)
+    c, x = t.execute("""
 
-    {{ { "type": "Uppercase" } }}
+    {{ { "type": "Uppercase" } }}r
     abcdefg
-    {{ { "type": "End" } }}    
+    {{ { "type": "End" } }}r    
    
     """)
-
+    print(x)
 
 
 if __name__ == "__main__":
